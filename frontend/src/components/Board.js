dce049a7f326bd061e59b951097e3d041514e979
Tristan Gill <racergill17@hotmail.com>
2023-06-16 02:53:56 -0400

    nadlc

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -5,6 +5,8 @@ const Board = ({ draft }) => {
   const [radiantTeam, setRadiantTeam] = useState("Team 1");
   const [direTeam, setDireTeam] = useState("Team 2");
   const [game, setGame] = useState("Game 1");
+  const [radiantScore, setRadiantScore] = useState("0");
+  const [direScore, setDireScore] = useState("0");
   const [nextAction, setNextAction] = useState();
 
   // surely a better way than this, but im tired
@@ -134,11 +136,11 @@ const Board = ({ draft }) => {
   const renderHeroAnimation = (heroName, isNext) => {
     return (
       <div className={`hero-animation-container ${isNext && 'hero-animation-container-selected'}`}>
-        {heroName && (
+        {false && (
           <video class="hero-animation" autoPlay={true} loop={true} muted={true} playsInline={true} preload="auto">
             <source
               type="video/webm"
-              src={`videos/npc_dota_hero_${heroName}.webm`}
+              src={`videos/npc_dota_hero_${"zuus"}.webm`}
             />
             <img
               src={`https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${heroName}.png`}
@@ -197,107 +199,148 @@ const Board = ({ draft }) => {
 
   return (
     <>
-      <div className='container'>
-        <div className='faction-section'>
-          <div className={radiantHasAnyPick ? 'pick-bar' : 'pick-bar-empty'}>
-            {renderHeroAnimation(draft?.team2?.pick0_class, nextAction === 'team2.pick0_class')}
-            {renderHeroAnimation(draft?.team2?.pick1_class, nextAction === 'team2.pick1_class')}
-            {renderHeroAnimation(draft?.team2?.pick2_class, nextAction === 'team2.pick2_class')}
-            {renderHeroAnimation(draft?.team2?.pick3_class, nextAction === 'team2.pick3_class')}
-            {renderHeroAnimation(draft?.team2?.pick4_class, nextAction === 'team2.pick4_class')}
-          </div>
-          
-          <div className={radiantHasAnyBan ? 'ban-bar' : 'ban-bar-empty'}>
-            {renderBanHero(draft?.team2?.ban0_class, nextAction === 'team2.ban0_class')}
-            {renderBanHero(draft?.team2?.ban1_class, nextAction === 'team2.ban1_class')}
-            {renderBanHero(draft?.team2?.ban2_class, nextAction === 'team2.ban2_class')}
-            {renderBanHero(draft?.team2?.ban3_class, nextAction === 'team2.ban3_class')}
-            {renderBanHero(draft?.team2?.ban4_class, nextAction === 'team2.ban4_class')}
-            {renderBanHero(draft?.team2?.ban5_class, nextAction === 'team2.ban5_class')}
-            {renderBanHero(draft?.team2?.ban6_class, nextAction === 'team2.ban6_class')}
-          </div>
-        </div>
-
-        <div className={`middle-section ${draft?.activeteam === 2 ? 'middle-section-left' : 'middle-section-right'}`}>
-          <div className='game-number'>
-            {game}
-          </div>
-
-          <div className='team-split'>
-            <div className='team-name'>{radiantTeam}</div>
-            <div className='side-arrow'>
-              {draft?.activeteam === 2 ? "<" : ">"}
-            </div>
-            <div className='team-name'>{direTeam}</div>
-          </div>
-
-          <div className='time-section'>
-            <div className='time-container'>
-              {draft?.activeteam === 2 && (
-                <>
-                  <div className='time'>
-                    {prettyPrintTime(draft?.activeteam_time_remaining)}
-                  </div>
-                  <div className='time-subheader'>ACTIVE</div>
-                </>
-              )}
-            </div>
-
-            <div className='time-container'>
-              {draft?.activeteam === 3 && (
-                <>
-                  <div className='time'>
-                    {prettyPrintTime(draft?.activeteam_time_remaining)}
-                  </div>
-                  <div className='time-subheader'>ACTIVE</div>
-                </>
-              )}
-            </div>
-          </div>
-
-          <div className='time-section'>
-            <div className='time-container'>
-              <div className='time'>
-                {prettyPrintTime(draft?.radiant_bonus_time)}
+      <div className="page">
+        <div className='container'>
+          <div className='faction-section'>
+            <div className="team-info">
+              <div className='d-flex ml-auto'>
+                <div className='team-name team-name-left'>
+                  {radiantTeam}
+                </div>
+                <div className='team-score'>
+                  {radiantScore}
+                </div>
               </div>
-              <div className='time-subheader'>RESERVE</div>
             </div>
 
-            <div className='time-container'>
-              <div className='time'>
-                {prettyPrintTime(draft?.dire_bonus_time)}
+            <div className='pick-bar'>
+              {renderHeroAnimation(draft?.team2?.pick0_class, nextAction === 'team2.pick0_class')}
+              {renderHeroAnimation(draft?.team2?.pick1_class, nextAction === 'team2.pick1_class')}
+              {renderHeroAnimation(draft?.team2?.pick2_class, nextAction === 'team2.pick2_class')}
+              {renderHeroAnimation(draft?.team2?.pick3_class, nextAction === 'team2.pick3_class')}
+              {renderHeroAnimation(draft?.team2?.pick4_class, nextAction === 'team2.pick4_class')}
+            </div>
+            
+            <div className='ban-bar'>
+              {renderBanHero(draft?.team2?.ban0_class, nextAction === 'team2.ban0_class')}
+              {renderBanHero(draft?.team2?.ban1_class, nextAction === 'team2.ban1_class')}
+              {renderBanHero(draft?.team2?.ban2_class, nextAction === 'team2.ban2_class')}
+              {renderBanHero(draft?.team2?.ban3_class, nextAction === 'team2.ban3_class')}
+              {renderBanHero(draft?.team2?.ban4_class, nextAction === 'team2.ban4_class')}
+              {renderBanHero(draft?.team2?.ban5_class, nextAction === 'team2.ban5_class')}
+              {renderBanHero(draft?.team2?.ban6_class, nextAction === 'team2.ban6_class')}
+            </div>
+          </div>
+
+          <div className="middle-section">
+            <div className='game-number'>
+              {game}
+            </div>
+            
+            <div className="time-section">
+              <div className={`side-arrow ${draft?.activeteam === 3 ? "side-arrow-hidden": ""}`}>
+                {"<"}
               </div>
-              <div className='time-subheader'>RESERVE</div>
+
+              <div>
+                <div className='time-container'>
+                  {draft?.activeteam === 2 && (
+                    <>
+                      <div className='time big-time'>
+                        {prettyPrintTime(draft?.activeteam_time_remaining)}
+                      </div>
+                    </>
+                  )}
+                </div>
+
+                <div className='time-container'>
+                  {draft?.activeteam === 3 && (
+                    <>
+                      <div className='time'>
+                        {prettyPrintTime(draft?.activeteam_time_remaining)}
+                      </div>
+                      <div className='time-subheader'>ACTIVE</div>
+                    </>
+                  )}
+                </div>
+              </div>
+
+              <div className={`side-arrow ${draft?.activeteam === 2 ? "side-arrow-hidden": ""}`}>
+                {">"}
+              </div>
+            </div>
+
+            <div className='time-section'>
+              <div className='time-container'>
+                <div className='time'>
+                  {prettyPrintTime(draft?.radiant_bonus_time)}
+                </div>
+                <div className='time-subheader'>RESERVE</div>
+              </div>
+
+              <div className='time-container'>
+                <div className='time'>
+                  {prettyPrintTime(draft?.dire_bonus_time)}
+                </div>
+                <div className='time-subheader'>RESERVE</div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className='faction-section'>
-          <div className={direHasAnyPick ? 'pick-bar' : 'pick-bar-empty'}>
-            {renderHeroAnimation(draft?.team3?.pick0_class, nextAction === 'team3.pick0_class')}
-            {renderHeroAnimation(draft?.team3?.pick1_class, nextAction === 'team3.pick1_class')}
-            {renderHeroAnimation(draft?.team3?.pick2_class, nextAction === 'team3.pick2_class')}
-            {renderHeroAnimation(draft?.team3?.pick3_class, nextAction === 'team3.pick3_class')}
-            {renderHeroAnimation(draft?.team3?.pick4_class, nextAction === 'team3.pick4_class')}
-          </div>
-          
-          <div className={direHasAnyBan ? 'ban-bar' : 'ban-bar-empty'}>
-            {renderBanHero(draft?.team3?.ban0_class, nextAction === 'team3.ban0_class')}
-            {renderBanHero(draft?.team3?.ban1_class, nextAction === 'team3.ban1_class')}
-            {renderBanHero(draft?.team3?.ban2_class, nextAction === 'team3.ban2_class')}
-            {renderBanHero(draft?.team3?.ban3_class, nextAction === 'team3.ban3_class')}
-            {renderBanHero(draft?.team3?.ban4_class, nextAction === 'team3.ban4_class')}
-            {renderBanHero(draft?.team3?.ban5_class, nextAction === 'team3.ban5_class')}
-            {renderBanHero(draft?.team3?.ban6_class, nextAction === 'team3.ban6_class')}
+          <div className='faction-section'>
+            <div className="team-info">
+              <div className='d-flex'>
+                <div className='team-score'>
+                  {direScore}
+                </div>
+                <div className='team-name team-name-right'>
+                  {direTeam}
+                </div>
+              </div>
+            </div>
+
+            <div className='pick-bar'>
+              {renderHeroAnimation(draft?.team3?.pick0_class, nextAction === 'team3.pick0_class')}
+              {renderHeroAnimation(draft?.team3?.pick1_class, nextAction === 'team3.pick1_class')}
+              {renderHeroAnimation(draft?.team3?.pick2_class, nextAction === 'team3.pick2_class')}
+              {renderHeroAnimation(draft?.team3?.pick3_class, nextAction === 'team3.pick3_class')}
+              {renderHeroAnimation(draft?.team3?.pick4_class, nextAction === 'team3.pick4_class')}
+            </div>
+            
+            <div className='ban-bar'>
+              {renderBanHero(draft?.team3?.ban0_class, nextAction === 'team3.ban0_class')}
+              {renderBanHero(draft?.team3?.ban1_class, nextAction === 'team3.ban1_class')}
+              {renderBanHero(draft?.team3?.ban2_class, nextAction === 'team3.ban2_class')}
+              {renderBanHero(draft?.team3?.ban3_class, nextAction === 'team3.ban3_class')}
+              {renderBanHero(draft?.team3?.ban4_class, nextAction === 'team3.ban4_class')}
+              {renderBanHero(draft?.team3?.ban5_class, nextAction === 'team3.ban5_class')}
+              {renderBanHero(draft?.team3?.ban6_class, nextAction === 'team3.ban6_class')}
+            </div>
           </div>
         </div>
       </div>
 
+      <div>
+        Radiant team
+      </div>
       <div>
         <input value={radiantTeam} name="radiantTeam" onChange={(e) => setRadiantTeam(e.target.value)} />
       </div>
       <div>
+        <input value={radiantScore} name="radiantScore" onChange={(e) => setRadiantScore(e.target.value)} />
+      </div>
+
+      <div>
+        Dire team
+      </div>
+      <div>
         <input value={direTeam} name="direTeam" onChange={(e) => setDireTeam(e.target.value)} />
+      </div>
+      <div>
+        <input value={direScore} name="direScore" onChange={(e) => setDireScore(e.target.value)} />
+      </div>
+      <div>
+        Title
       </div>
       <div>
         <input value={game} name="game" onChange={(e) => setGame(e.target.value)} />
